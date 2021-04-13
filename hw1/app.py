9f0c624eef05245674afc7e4ee2f8d56c113fc30
ymykytyn <ymykytyn@rebbix.com>
2021-04-13 19:08:40 +0300

    add linters

--- hw1/app.py
+++ hw1/app.py
@@ -6,27 +6,31 @@ from datetime import (timedelta, datetime)
 
 config = json.load(open('config.json', 'r'))
 
+
 def authorize():
     auth_token = requests.post(url=f"{config['base_url']}{config['auth_url']}",
-                           json=config['auth_credentials']
-                          )
-    auth_header = {'Authorization':f"JWT {auth_token.json()['access_token']}"}
+                               json=config['auth_credentials']
+                               )
+    auth_header = {'Authorization': f"JWT {auth_token.json()['access_token']}"}
     return auth_header
+
 
 def get_data(day, auth_header):
     response = requests.get(f"{config['base_url']}{config['data_url']}",
-                    json={'date':day},
-                    headers=auth_header)
+                            json={'date': day},
+                            headers=auth_header)
     return response
+
 
 def main():
     today = datetime.now().date()
-    days = [( today - timedelta(days=step)).strftime('%Y-%m-%d')
+    days = [(today - timedelta(days=step)).strftime('%Y-%m-%d')
             for step in range(config['days_to_load'])]
-    
-    if not os.path.exists('data'): os.mkdir('data')
+
+    if not os.path.exists('data'):
+        os.mkdir('data')
     auth_header = authorize()
-    
+
     print("Writing data")
     for day in days:
         response = get_data(day, auth_header)
@@ -36,11 +40,13 @@ def main():
         data = response.json()
 
         dirname = f"data/out-of-stock-{day}"
-        if not os.path.exists(dirname): os.mkdir(dirname)
+        if not os.path.exists(dirname):
+            os.mkdir(dirname)
         with open(f"{dirname}/data.json", 'w') as datafile:
             json.dump(data, datafile)
         print(f"date: {day}")
     print("done")
+
 
 if __name__ == '__main__':
     main()
